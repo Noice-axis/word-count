db946603fa1f2817bdd7076036f9cf4f9a258014
Noice-axis <2721855217@qq.com>
2020-05-20 00:01:39 +0800

    cpp文件

---
+++ wordcount.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc,char *argv[])
+{
+	int wordcount=0;
+	int signalcount=0;
+	int temp;
+	int tempt=NULL;
+	FILE *pt;
+	if(argc==1)
+	{
+		printf("请使用格式:%s",argv[0]);
+		exit(0);
+	}
+	if((pt=fopen(argv[1],"r+"))==NULL)
+	{
+		printf("%s文件打开失败\n",argv[1]);
+		exit(1);
+	}
+	else
+		printf("yes\n");
+	fseek(pt,0L,0);
+	while(!feof(pt))
+	{
+		temp=fgetc(pt);
+		printf("h\n");
+		if(temp==' '||temp=='\n')
+			signalcount++;
+		if(temp==' '||temp=='\n'||temp==',')
+			wordcount++;
+		tempt=temp;
+	}
+	printf("wordnumber:%d\n",wordcount);
+	printf("signalnumber:%d\n",signalcount);
+	fclose(pt);
+	return 0;
+}
